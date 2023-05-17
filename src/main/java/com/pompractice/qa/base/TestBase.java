ddec9593f9f28e3fa94a87a971ba5320e5928437
vinothsubrama.laksh <vinothsubrama.laksh@hcl.com>
2023-05-17 13:41:02 +0530

    commit fresh

--- src/main/java/com/pompractice/qa/base/TestBase.java
+++ src/main/java/com/pompractice/qa/base/TestBase.java
@@ -1,5 +1,25 @@
 package com.pompractice.qa.base;
 
-public class TestBase   {
+import org.openqa.selenium.WebDriver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestBase   {
+        static WebDriver driver;
+        static Properties prop;
+           public TestBase(){
+                try{
+                    prop=new Properties();
+                    FileInputStream fi=new FileInputStream("/Users/vinothsubrama.laksh/eclipse-workspace/POMPractice/src/main/java/com/pompractice"+"/qa/config.properties");
+                    prop.load(fi);
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+           }
 }
--- src/main/java/com/pompractice/qa/pages/LoginPage.java
+++ src/main/java/com/pompractice/qa/pages/LoginPage.java
@@ -1,4 +1,23 @@
 package com.pompractice.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+    @FindBy(xpath = "/html/body/div[2]/div/div/div/div/div[2]/div/form/div[1]/input")
+    WebElement mobileNum;
+
+//    @FindBy(xpath = "")
+//    WebElement password;
+
+    @FindBy(xpath = "//button[contains(text(), 'Request OTP')]")
+    WebElement reqOTPBtn;
+
+
+    public void login(){
+        mobileNum.sendKeys("9901364570");
+        reqOTPBtn.click();
+
+    }
 }
